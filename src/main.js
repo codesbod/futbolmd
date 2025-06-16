067b7dc363d3adefa02e075d8e4e3bfe23ec8b2a
sergio1008 <sergiobernalc@outlook.com>
2025-06-17 00:11:55 +0200

    [SABC] Avance traducciones

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import './assets/main.css'
+import { createI18n } from 'vue-i18n'
+
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -11,9 +13,13 @@ import "bootstrap-icons/font/bootstrap-icons.min.css";
 
 // Import our custom CSS
 import '/src/assets/styles.scss'
+import { messages } from './i18n'
 
 const app = createApp(App)
 
+const i18n = createI18n(messages)
+
 app.use(createPinia())
 app.use(router)
+app.use(i18n)
 app.mount('#app')
